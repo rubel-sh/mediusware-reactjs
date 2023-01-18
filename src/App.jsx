938a9cd0b9a9b58e2f2c300e5a6c8b3217675012
Rubel Hossain <rubrex96@hotmail.com>
2023-01-19 03:29:12 +0600

    Modal added  for  problem 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Problem1 from "./components/Problem-1.jsx";
 import Menu from "./components/Menu.jsx";
 import Problem2 from "./components/Problem-2.jsx";
 import Index from "./components/Index.jsx";
+import "./globalStyles.css";
 
 function App() {
   return (
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import ModalA from "./ModalA";
 
 const Problem2 = () => {
+  const [modalShow, setModalShow] = useState(false);
+  const [modalInfo, setModalInfo] = useState([]);
+  const [modalType, setModalType] = useState("");
+  // const [modalBInfo, setModalBInfo] = useState([]);
 
-    return (
+  const handleModalA = () => {
+    setModalShow(true);
+    setModalType("Modal A");
+    // Call the API
+    // Set data to setModalInfo state
+    // send the state to modal A
+  };
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+  const handleModalB = () => {
+    setModalShow(true);
+    setModalType("Modal B");
+    // Call the API
+    // Set data to setModalInfo state
+    // send the state to modal A
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <Button
+            type="button"
+            variant="outline-primary"
+            onClick={handleModalA}
+          >
+            All Contacts
+          </Button>
+          <Button
+            type="button"
+            variant="outline-warning"
+            onClick={handleModalB}
+          >
+            US Contacts
+          </Button>
         </div>
-    );
+      </div>
+
+      <ModalA
+        modalInfo={modalInfo}
+        modalType={modalType}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </div>
+  );
 };
 
 export default Problem2;
